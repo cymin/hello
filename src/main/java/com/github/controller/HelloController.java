f8635764f5799d86e89b70bf4e08917ca49a1c92
cymin <cymin@cymindeMBP.lan>
2018-06-14 17:13:54 +0800

    Add external application configuration

--- src/main/java/com/github/controller/HelloController.java
+++ src/main/java/com/github/controller/HelloController.java
@@ -3,6 +3,7 @@ package com.github.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,9 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     private static final Logger LOGGER = LoggerFactory.getLogger(HelloController.class);
 
+    @Value("${hello.username:world}")
+    private String username;
+
     @GetMapping("/")
     public String index() {
-        LOGGER.info("Hello.....");
-        return "hello";
+        LOGGER.info("Hello, {}", username);
+        return "hello, " + username;
     }
 }
